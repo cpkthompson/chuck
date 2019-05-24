5c2f2c276aebfd21a62e406274d985ed1c4c7642
Charles Thompson <thompsoncharles@live.com>
2019-05-24 21:54:53 +0000

    small fix

--- chuck/urls.py
+++ chuck/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('workspace.urls', namespace='workspace')),
+    path('', include('workspace.urls')),
 ]
