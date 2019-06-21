36eee20dcc765f4f709b014d4ecb8680bb239633
Philisiah <sphilisiah@gmail.com>
2019-06-21 16:15:22 +0000

    fix: views

--- workspace/views.py
+++ workspace/views.py
@@ -38,7 +38,8 @@ def workspace(request):
 @csrf_exempt
 def ide_user(request, workspace_name, time, url):
     my_workspace_name = workspace_name
-    my_workspace = IdeUser.objects.create(workspace_name=my_workspace_name, end_time=time, url=url)
+    my_time = datetime.datetime.fromtimestamp(time)
+    my_workspace = IdeUser.objects.create(workspace_name=my_workspace_name, end_time=my_time, url=url)
     return HttpResponse('OK')
 
 
